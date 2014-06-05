d1eb97fe7bd03eae9a007df06897fbede170dc3d
epipe-com <epipe-com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-05 19:04:48 +0000

    Use get_site_url() instead of get_home_url().
    git-svn-id: https://plugins.svn.wordpress.org/the-simplest-favicon/trunk@927306 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- the-simplest-favicon.php
+++ the-simplest-favicon.php
@@ -4,14 +4,14 @@ Plugin Name: The Simplest Favicon
 Plugin URI: http://wordpress.org/extend/plugins/the-simplest-favicon/
 Description: Adds a favicon link to the document head section.
 Author: EPIPE Communications
-Version: 1.0
+Version: 1.1
 Author URI: http://epipe.com/
 License: GPLv2 or later
 */
 
 function the_simplest_favicon_head_action() {
 	echo "<link rel='shortcut icon' href='"
-	    . get_home_url()
+	    . get_site_url()
 	    . "/favicon.ico' />\n";
 }
 
